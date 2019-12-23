ad0f1e7b38514bf59b0f76c5e960d2b64c2d2b31
Łukasz Fronczyk <lukasz.fronczyk@g1ant.com>
2019-12-23 18:14:09 +0100

    AB#832 - addon structure base

---
+++ G1ANT.Addon.JavaUI/MainAddon.cs
@@ -0,0 +1,12 @@
+﻿using G1ANT.Language;
+
+namespace G1ANT.Addon.JavaUI
+{
+    [Addon(Name = "JavaUI", Tooltip = "Addon which support java application automations")]
+    [Copyright(Author = "G1ANT Robot Ltd", Copyright = "G1ANT Robot Ltd", Email = "hi@g1ant.com", Website = "www.g1ant.com")]
+    [License(Type = "LGPL", ResourceName = "License.txt")]
+    [CommandGroup(Name = "ui", Tooltip = "Command allows java applications automation")]
+    public class MainAddon : Language.Addon
+    {
+    }
+}
