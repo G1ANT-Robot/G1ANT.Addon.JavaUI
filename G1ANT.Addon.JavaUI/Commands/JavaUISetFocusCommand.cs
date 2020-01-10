abe13ee0ac7d6272656115a92f255ccae673f2bb
Łukasz Fronczyk <lukasz.fronczyk@g1ant.com>
2020-01-10 18:08:30 +0100

    AB#832 - added a parameter to javaui.setfocus that allows to bring the app window to top before applying focus

--- G1ANT.Addon.JavaUI/Commands/JavaUISetFocusCommand.cs
+++ G1ANT.Addon.JavaUI/Commands/JavaUISetFocusCommand.cs
@@ -1,6 +1,8 @@
 using G1ANT.Addon.JavaUI;
+using G1ANT.Addon.JavaUI.Models;
 using G1ANT.Addon.JavaUI.Services;
 using G1ANT.Language;
+using System.Threading;
 using WindowsAccessBridgeInterop;
 
 namespace G1ANT.Addon.UI
@@ -15,6 +17,9 @@ namespace G1ANT.Addon.UI
         {
             [Argument(Required = true, Tooltip = "Desktop Java application UI element to apply the focus")]
             public JavaPathStructure Path { get; set; }
+
+            [Argument(Required = true, Tooltip = "If set to true then brings to front window associated with the node")]
+            public BooleanStructure BringToFront { get; set; }
         }
 
         public JavaUISetFocusCommand(AbstractScripter scripter) : base(scripter)
@@ -22,9 +27,25 @@ namespace G1ANT.Addon.UI
             pathService = new PathService();
         }
 
+        private static bool IsWindowMinimized(NodeModel windowNode)
+        {
+            return windowNode.X + windowNode.Width < 0 && windowNode.Y + windowNode.Height < 0;
+        }
+
         public void Execute(Arguments arguments)
         {
             var node = pathService.GetByXPath(arguments.Path.Value);
+            if (arguments.BringToFront.Value)
+            {
+                var windowNode = node.GetParentWindow();
+                var accessibleWindow = (AccessibleWindow)windowNode.Node;
+
+                RobotWin32.BringWindowToFront(accessibleWindow.Hwnd);
+
+                if (IsWindowMinimized(windowNode))
+                    Thread.Sleep(2000);
+            }
+
             node.RequestFocus();
         }
     }
