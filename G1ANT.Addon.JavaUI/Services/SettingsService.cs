237c2c907a74aabfdd9c9ea43bb2b939469dc9c9
Łukasz Fronczyk <lukasz.fronczyk@g1ant.com>
2020-01-16 13:14:08 +0100

    AB#832 - patch for DI process (LoadMetadata at Azure Pipelines caused Panel creation, which caused JABFactory to initialize JAB with not initialized SettingsContainer => NRE)

--- G1ANT.Addon.JavaUI/Services/SettingsService.cs
+++ G1ANT.Addon.JavaUI/Services/SettingsService.cs
@@ -4,6 +4,6 @@ namespace G1ANT.Addon.JavaUI.Services
 {
     public class SettingsService : ISettingsService
     {
-        public string GetUserDocsAddonFolder() => AbstractSettingsContainer.Instance.UserDocsAddonFolder.FullName;
+        public string GetUserDocsAddonFolder() => AbstractSettingsContainer.Instance?.UserDocsAddonFolder.FullName ?? "";
     }
 }
